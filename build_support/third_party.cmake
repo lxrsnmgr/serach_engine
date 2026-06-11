587725e7c2ccbd4d9d2bb242081776bdf0d6f944
LXRSNMGR <m15648587670@163.com>
2026-06-11 15:43:42 +0800

    make文件

---
+++ build_support/third_party.cmake
@@ -0,0 +1,3 @@
+# reference: https://github.com/netcan/config-loader/blob/master/third-party/ThirdParty.cmake
+
+# add_subdirectory(third_party/re2)
---
+++ test/cache/cache_test.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../../src/include/cache/lru_cache.h"
+
+using std::cout;
+using std::endl;
+
+int main(){
+    simple::LruCache lc(5);
+}
